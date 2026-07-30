eb158571bb80a29c8c483f8c56c143c24b8f5a40
DianaMarcelaarenas <arenastobond@gmail.com>
2026-07-30 01:44:58 -0500

    Se agrega entidad Student

---
+++ src/main/java/co/edu/cesde/domain/models/Student.java
@@ -0,0 +1,62 @@
+package co.edu.cesde.domain.models;
+
+import java.time.LocalDate;
+
+public class Student {
+    Long id;
+    String firstName;
+    String lastName;
+    String email;
+    LocalDate birthDate;
+
+    public Student(Long id, String firstName, String lastName, String email, LocalDate birthDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    public Student() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
