7f1a9101d07fe62d7cb3b66404dcd45497482d53
DianaMarcelaarenas <arenastobond@gmail.com>
2026-08-10 08:33:55 -0500

    feat: se agregan Servicios

--- src/main/java/co/edu/cesde/application/Repository/CourseRepository.java
+++ src/main/java/co/edu/cesde/application/Repository/CourseRepository.java
@@ -7,7 +7,7 @@ import co.edu.cesde.domain.models.Course;
 import java.util.List;
 import java.util.Optional;
 
-public interface CurseRepository {
+public interface CourseRepository {
     Course save(Course course);
     Optional<Course> findById(Long id);
     List<Course> findAll();
